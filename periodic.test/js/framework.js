ec3485a772c31a20ea5787c22c5679026fb14a58
Tyler Pippin <58650245+pylotylo@users.noreply.github.com>
2020-09-08 18:42:41 -0700

    clean up

--- periodic.test/js/framework.js
+++ periodic.test/js/framework.js
@@ -1,23 +1,21 @@
-var stuff = [];
+let json;
+let elements = [];
+let elementSize = window.innerWidth / 22;
+
+function preload() {
+    let url = 'elements.json';
+    json = loadJSON(url);
+}
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     background(60);
-    let url = 'elements.json';
-    info = loadJSON(url);
-    append(stuff, info);
+    for (var key in json) {
+        append(elements, json[key]);
+    }
 }
 
-console.log(stuff);
-var elements = stuff[0];
-console.log(elements);
-
-function drawElements() {
-    const elementSize = window.innerWidth / 22;
-
-    background(60);
-    stroke(255);
-    strokeWeight(1);
+function drawTable() {
 
     for (let i = 0; i < elements.length; i++) {
         let currElement = elements[i];
@@ -26,56 +24,77 @@ function drawElements() {
         let symbol = currElement.symbol;
         let atomic = currElement.atomic;
         let mass = currElement.mass;
-        let CESes = elementSize / 3.5;
-        let ESces = elementSize / 6.05;
+        let _fill = currElement.fill;
 
-        if (mouseX > x &&
-            mouseX < x + elementSize &&
-            mouseY > y &&
-            mouseY < y + elementSize) {
-
-            fill(30);
-
-            //change color and stroke weight for hovered element
-            stroke(255, 0, 0);
-            strokeWeight(3);
-        }
-
-
-        //draw box around element
-        fill('#' + currElement.fill);
-        square(x, y, elementSize);
-        // change color and stroke weight BACK for non-hovered elements
-        stroke(255, 255, 255);
-        strokeWeight(1);
-        //draw a corner squar instead of a corner triangle
-        noFill();
-        rect(x, y, (elementSize / 3), (elementSize / 4), 0, 0, (elementSize / 15), 0);
-        //draw element symbol text
-        textAlign(CENTER);
-        fill(255);
-        textSize(CESes);
-        text(symbol,
-            x + elementSize / 2,
-            y + elementSize / 2);
-        //draw atomic number
-        textAlign(CENTER);
-        fill(255);
-        textSize(ESces);
-        text(atomic,
-            x + elementSize / 6,
-            y + elementSize / 5);
-        //draw atomic mass
-        textAlign(CENTER);
-        fill(255);
-        textSize(ESces);
-        text(mass,
-            x + elementSize / 2,
-            y + elementSize / 1.25);
+        drawElement(x, y, atomic, symbol, mass, _fill);
     }
+}
+
+function drawElement(x, y, atomic, symbol, mass, _fill) {
+
+    let CESes = elementSize / 3.5;
+    let ESces = elementSize / 6.05;
+
+    stroke(255);
+    strokeWeight(1);
+
+    //draw box around element
+    fill('#' + _fill);
+    square(x, y, elementSize);
+    // change color and stroke weight BACK for non-hovered elements
+    stroke(255, 255, 255);
+    strokeWeight(1);
+    //draw a corner squar instead of a corner triangle
+    noFill();
+    rect(x, y, (elementSize / 3), (elementSize / 4), 0, 0, (elementSize / 15), 0);
+    //draw element symbol text
+    textAlign(CENTER);
+    fill(255);
+    textSize(CESes);
+    text(symbol,
+        x + elementSize / 2,
+        y + elementSize / 2);
+    //draw atomic number
+    textAlign(CENTER);
+    fill(255);
+    textSize(ESces);
+    text(atomic,
+        x + elementSize / 6,
+        y + elementSize / 5);
+    //draw atomic mass
+    textAlign(CENTER);
+    fill(255);
+    textSize(ESces);
+    text(mass,
+        x + elementSize / 2,
+        y + elementSize / 1.25);
 
 }
 
 function draw() {
-    drawElements();
+    drawTable();
+}
+
+function mouseHover() {
+    for (let i = 0; i < elements.length; i++) {
+        let currElement = elements[i];
+        let x = (2 + currElement.x) * elementSize;
+        let y = (2 + currElement.y) * elementSize;
+        let symbol = currElement.symbol;
+        let atomic = currElement.atomic;
+        let mass = currElement.mass;
+        let _fill = currElement.fill;
+        if (mouseX > x &&
+            mouseX < x + elementSize &&
+            mouseY > y &&
+            mouseY < y + elementSize) {
+            validate = true;
+        } else {
+            validate = false;
+        }
+        while (validate = true) {
+            _fill = 30;
+            drawElement(x, y, atomic, symbol, mass, _fill);
+        }
+    }
 }
